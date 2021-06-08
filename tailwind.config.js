4a3a1ffed3af7bcde3eb976da7191b5066a9c992
jorge <jorge@syndi.health>
2021-06-08 14:34:37 +0100

    html update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
+require("typeface-poppins");
+
 module.exports = {
   mode: 'jit',
   purge: ["index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+     'display': ['Poppins'],
+     'body': ['Poppins'],
+     'sans': ['Poppins'],
+    }
   },
   variants: {
     extend: {},
